56a68a92a0266df64a4a55f7701ef53963cf17e0
Akari <96369233+varangian-core@users.noreply.github.com>
2025-05-22 00:02:50 -0700

    Add smooth animation controls

--- mapanimation.js
+++ mapanimation.js
@@ -11,8 +11,10 @@ let map = new mapboxgl.Map({
 const marker = new mapboxgl.Marker();
 marker.setLngLat([-71.093729, 42.359244]).addTo(map);
 
-// Store the timeout id so the animation can be cancelled
-let timerId = null;
+// animation control variables
+let animationId = null;
+let paused = false;
+let progress = 0;
 
 
 //Layer elements
@@ -27,50 +29,104 @@ let timerId = null;
 }
 //Current coordinates
 // TODO: replace with api data
-    const busStops = [
-        [-71.093729, 42.359244],
-        [-71.094915, 42.360175],
-        [-71.0958, 42.360698],
-        [-71.099558, 42.362953],
-        [-71.103476, 42.365248],
-        [-71.106067, 42.366806],
-        [-71.108717, 42.368355],
-        [-71.110799, 42.369192],
-        [-71.113095, 42.370218],
-        [-71.115476, 42.372085],
-        [-71.117585, 42.373016],
-        [-71.118625, 42.374863],
-    ];
+const busStops = [
+    [-71.093729, 42.359244],
+    [-71.094915, 42.360175],
+    [-71.0958, 42.360698],
+    [-71.099558, 42.362953],
+    [-71.103476, 42.365248],
+    [-71.106067, 42.366806],
+    [-71.108717, 42.368355],
+    [-71.110799, 42.369192],
+    [-71.113095, 42.370218],
+    [-71.115476, 42.372085],
+    [-71.117585, 42.373016],
+    [-71.118625, 42.374863],
+];
+
+map.on('load', () => {
+    map.addSource('route', {
+        type: 'geojson',
+        data: {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+                type: 'LineString',
+                coordinates: busStops
+            }
+        }
+    });
+    map.addLayer({
+        id: 'route',
+        type: 'line',
+        source: 'route',
+        layout: {
+            'line-join': 'round',
+            'line-cap': 'round'
+        },
+        paint: {
+            'line-color': '#888',
+            'line-width': 4
+        }
+    });
+});
 
 
-    // counter here represents the index of the current bus stop
-var counter = 0;
+// counter represents the index of the current bus stop
+let counter = 0;
 
-function reset(){
-    // stop any pending animation
-    if (timerId) {
-        clearTimeout(timerId);
-        timerId = null;
+function reset() {
+    if (animationId) {
+        cancelAnimationFrame(animationId);
+        animationId = null;
     }
     counter = 0;
+    progress = 0;
+    paused = false;
     marker.setLngLat(busStops[counter]);
-    // allow the user to start the animation again
     document.getElementById('MIT_path').disabled = false;
 }
 
-function move() {
-    //disable the button while animation is running
-    document.getElementById('MIT_path').disabled = true;
+function getSpeed() {
+    const el = document.getElementById('speed');
+    return el ? parseFloat(el.value) : 0.01;
+}
 
-    if (counter >= busStops.length) {
-        // animation finished
+function animate() {
+    if (paused) {
+        animationId = null;
+        return;
+    }
+    if (counter >= busStops.length - 1) {
         document.getElementById('MIT_path').disabled = false;
+        animationId = null;
         return;
     }
 
-    marker.setLngLat(busStops[counter]);
-    counter++;
+    const start = busStops[counter];
+    const end = busStops[counter + 1];
+    progress += getSpeed();
 
-    // schedule next stop
-    timerId = setTimeout(move, 500);
+    if (progress >= 1) {
+        progress = 0;
+        counter++;
+    }
+
+    const lng = start[0] + (end[0] - start[0]) * progress;
+    const lat = start[1] + (end[1] - start[1]) * progress;
+    marker.setLngLat([lng, lat]);
+
+    animationId = requestAnimationFrame(animate);
+}
+
+function move() {
+    document.getElementById('MIT_path').disabled = true;
+    paused = false;
+    if (!animationId) {
+        animationId = requestAnimationFrame(animate);
+    }
+}
+
+function pauseAnim() {
+    paused = true;
 }
